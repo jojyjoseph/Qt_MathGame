9467468ce182fd146dea8db34f4948a32d24f401
Joseph George (BRT-SG) <joseph.george@Brtchip.local>
2020-09-28 21:57:35 +0800

    Updated the size of the application

--- MathGame/main.cpp
+++ MathGame/main.cpp
@@ -1,10 +1,15 @@
 #include "MathGame.h"
 #include <QtWidgets/QApplication>
+#include <QDesktopWidget>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MathGame w;
-    w.show();
+    MathGame mainWin;
+	mainWin.resize(QApplication::desktop()->availableGeometry(QApplication::desktop()->primaryScreen()).size() - QSize(64, 64)); // TODO: First start-up only!
+	mainWin.move(32, 32); // TODO: First start-up only!
+	mainWin.show();
+
+
     return a.exec();
 }
